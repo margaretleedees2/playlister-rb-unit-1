eaede94c4bc53f69a856b68d9361910d0df4a5fd
emilyxxie <emilyxxie@gmail.com>
2013-10-16 21:18:33 -0400

    I THINK THE PARSING WORKS

--- lib/models/artist.rb
+++ lib/models/artist.rb
@@ -10,38 +10,24 @@ class Artist
     # @songs << 
   end
 
-  # This also applies to genres, replace artists with genres:
-  # An artist only needs to track it's songs in an array.
-  # The artist's genres in from it's #genres method can be 
-  # determined by looking at it's collection of songs and
-  # collecting those genres.
-
-  def self.find_or_create_by_name(name)
-    # Try to find a genre with this name
-    self.find_by_name(name) || self.create_by_name(name) 
-    # otherwise create it
+  def self.find_or_create_by_name(name, song)
+    self.find_by_name(name, song) || self.create_by_name(name, song) 
   end
 
-  def self.find_by_name(name)
-    All.detect{|a| a.name == name}
+  def self.find_by_name(name, song)
+    if All.find{|a| name if a.name == name} != nil
+      All.find{|a| name if a.name == name}.songs << song
+    end
   end
 
   def self.create_by_name(name, song)
-    Artist.new.tap{|a| a.name = name; a.song = song}
+    Artist.new.tap{|a| a.name = name; a.songs = [song]}
   end
 
   def genres
     songs.collect{|s| s.genre}.uniq
     #this returns an array of unique genres
   end
-
-  # def self.find_or_create_by(name)
-  #   Artist.all.each do |artist|
-  #     puts name if artist.name == name
-  #   end
-  #   #we're trying to see if this artist already exists as an instance
-  #   #see if artist exists or not, if it does not exist, instantiate
-
 
   def self.reset_artists
     All.clear
@@ -64,3 +50,18 @@ class Artist
     song.artist = self
   end
 end
+
+  # This also applies to genres, replace artists with genres:
+  # An artist only needs to track it's songs in an array.
+  # The artist's genres in from it's #genres method can be 
+  # determined by looking at it's collection of songs and
+  # collecting those genres.
+
+
+  # def self.find_or_create_by(name)
+  #   Artist.all.each do |artist|
+  #     puts name if artist.name == name
+  #   end
+  #   #we're trying to see if this artist already exists as an instance
+  #   #see if artist exists or not, if it does not exist, instantiate
+
--- lib/models/libraryparser.rb
+++ lib/models/libraryparser.rb
@@ -18,20 +18,6 @@ class LibraryParser
     end
   end
 
-  # def build_objects(artist_name, song_name, genre_name)
-  #   artist = Artist.new
-  #   artist.name = artist_name
-
-  #   song = Song.new
-  #   song.name = song_name
-
-  #   genre = Genre.new
-  #   genre.name = genre_name
-
-  #   song.genre = genre
-  #   artist.songs << songs
-  # end
-
   def build_objects(artist_name, song_name, genre_name)
     song = Song.new
     song.name = song_name
@@ -40,22 +26,10 @@ class LibraryParser
     song.genre = Genre.find_or_create_by_name(genre_name)
   end
 
-
-# new.tap{|a| a.name = artist_name}  
-
 end  
 
 binding.pry
 parser = LibraryParser.new('../../data')
-
-#artists have songs
-#songs have genres, and artists.
-#genres have songs
-
-
-      # SONG_ARTIST_GENRE[0] = Artist.new(song_array[0], song_array[1])
-      # SONG_ARTIST_GENRE[1] = Song.new
-      # SONG_ARTIST_GENRE[2] = Genre.new
 
 
 
